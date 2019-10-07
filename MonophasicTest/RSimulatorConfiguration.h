0a11c5338e42c25d9c1b1c5b6ceb6e5da99decc4
Omar Duran <omaryesiduran@gmail.com>
2019-10-07 20:50:18 -0300

    First commit.

---
+++ MonophasicTest/RSimulatorConfiguration.h
@@ -0,0 +1,49 @@
+//
+//  RSimulatorConfiguration.hpp
+//  MonophasicTest
+//  Created by Jose on 27/8/19.
+//
+
+#ifndef RSimulatorConfiguration_hpp
+#define RSimulatorConfiguration_hpp
+
+#include <stdio.h>
+#include "includes.h"
+
+class RSimulatorConfiguration{
+    
+private:
+    TPZGeoMesh *fGmesh;
+    TPZMultiphysicsCompMesh *multmesh;
+    SimulationCase fsim_case;
+    
+public:
+    RSimulatorConfiguration();
+    RSimulatorConfiguration(SimulationCase sim_case);
+    
+    RSimulatorConfiguration(TPZGeoMesh *gmesh);
+    RSimulatorConfiguration(RSimulatorConfiguration *conf);
+    
+    void CreateGeomesh(int nx, int ny, double l, double h, MElementType type);
+    TPZGeoMesh * GetGeomesh();
+    
+    TPZCompMesh * CreateFluxCmesh(TPZGeoMesh * gmesh, int order);
+    TPZCompMesh * CreatePressureCmesh(TPZGeoMesh * gmesh, int order);
+    TPZCompMesh * CreateTransportMesh(TPZMultiphysicsCompMesh *cmesh, int ref);
+    void CreateTransportElement(int p_order, TPZCompMesh *cmesh, TPZGeoEl *gel, bool is_BC);
+    TPZMultiphysicsCompMesh *CreateMultiPhysicsCompMesh(TPZGeoMesh * gmesh);
+       TPZMultiphysicsCompMesh * MPTransportMesh(TPZMultiphysicsCompMesh * mixed, TPZManVector<TPZCompMesh *> meshvec);
+    
+    TPZAnalysis * CreateAnalysis(TPZMultiphysicsCompMesh * cmesh_c,  bool must_opt_band_width_Q, int number_threads, bool UsePardiso_Q);
+    
+    void PrintCmesh(int mesh_index, std::ofstream &file_name);
+    void PrintGmesh( std::ofstream &file_name);
+    void PosProcess();
+    void Run();
+    void InsertTransportInterfaceElements(TPZMultiphysicsCompMesh *cmesh);
+    void InsertInterfacesBetweenElements(int transport_matid, TPZCompMesh * cmesh, std::vector<int> & cel_indexes);
+    TPZAnalysis * CreateTransportAnalysis(TPZCompMesh * cmesh, SimulationCase & sim_data);
+    TPZFMatrix<STATE> TimeForward(TPZAnalysis * tracer_analysis, int & n_steps, REAL & dt, TPZFMatrix<STATE> & M_diag);
+};
+
+#endif /* RSimulatorConfiguration_hpp */
---
+++ MonophasicTest/main.cpp
@@ -0,0 +1,124 @@
+
+//$Id: main.cc,v 1.21 2010-07-22 17:43:43 caju Exp $
+#ifdef HAVE_CONFIG_H
+#include <pz_config.h>
+#endif
+
+#include "pzgeoel.h"
+#include "pzgnode.h"
+#include "pzgmesh.h"
+#include "pzcmesh.h"
+#include "pzintel.h"
+#include "pzcompel.h"
+#include "pzfmatrix.h"
+#include "pzvec.h"
+#include "pzmanvector.h"
+#include "pzstack.h"
+
+#include "pzanalysis.h"
+#include "pzfstrmatrix.h"
+#include "pzskylstrmatrix.h"
+#include "pzstepsolver.h"
+#include "pzgeopyramid.h"
+#include "TPZGeoLinear.h"
+
+#include <TPZRefPattern.h>
+
+#include "TPZMaterial.h"
+#include "pzelasmat.h"
+#include "pzlog.h"
+
+#include "pzgengrid.h"
+
+#include <time.h>
+#include <stdio.h>
+
+#include <math.h>
+
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <sstream>
+
+#include <set>
+#include <map>
+#include <vector>
+#include "TPZRefPatternDataBase.h"
+#include "TPZRefPatternTools.h"
+#include "TPZVTKGeoMesh.h"
+#include "TPZExtendGridDimension.h"
+#include "pzshapetriang.h"
+#include "pzshapequad.h"
+
+#include "pzpoisson3d.h"
+#include "mixedpoisson.h"
+#include "pzbndcond.h"
+#include "pzbuildmultiphysicsmesh.h"
+#include "TPZMultiphysicsCompMesh.h"
+#include "pznoderep.h"
+#include "pzshapetetra.h"
+#include "RSimulatorConfiguration.h"
+#include "pzshapepiram.h"
+
+void case1_Axi();
+
+
+int main(){
+    case1_Axi();
+    
+    return 0;
+}
+
+void case1_Axi(){
+    
+    SimulationCase sim;
+    sim.UsePardisoQ=true;
+    sim.IsHybrid=true;
+    sim.n_threads = 24;
+    sim.omega_ids.push_back(1);
+    sim.omega_dim.push_back(2);
+    sim.permeabilities.push_back(1.0);
+    sim.porosities.push_back(1.0);
+
+    /// C inlet value
+    sim.c_inlet = 1.0;
+    
+    int bc_non_flux = -1;
+    int bc_inlet  = -2;
+    int bc_non_flux2 = -3;
+    int bc_outlet = -4;
+   
+    sim.gamma_ids.push_back(bc_non_flux);
+    sim.gamma_dim.push_back(1);
+    sim.gamma_ids.push_back(bc_inlet);
+    sim.gamma_dim.push_back(1);
+    sim.gamma_ids.push_back(bc_non_flux2);
+    sim.gamma_dim.push_back(1);
+    sim.gamma_ids.push_back(bc_outlet);
+    sim.gamma_dim.push_back(1);
+   
+    
+    
+    int bc_type_D = 0;    //    D = 0;
+    int bc_type_N = 1;    //    N = 1;
+    REAL p_inlet  = 240.818;
+    REAL p_outlet = 50.0;
+    REAL qn       = 0.0;
+    
+    sim.type.push_back(bc_type_N);
+    sim.type.push_back(bc_type_D);
+    sim.type.push_back(bc_type_N);
+    sim.type.push_back(bc_type_D);
+ 
+    sim.vals.push_back(qn);
+    sim.vals.push_back(p_inlet);
+    sim.vals.push_back(qn);
+    sim.vals.push_back(p_outlet);
+    
+    
+    RSimulatorConfiguration caseSim(sim);
+    caseSim.Run();
+    
+    int ok;
+}
+
