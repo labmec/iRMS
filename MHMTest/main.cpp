d8d97eff773435f0ca4aa4b3f5db55a39425ecc6
Omar Durán <omaryesiduran@gmail.com>
2020-06-23 11:29:03 +0200

    Update main.cpp

--- MHMTest/main.cpp
+++ MHMTest/main.cpp
@@ -765,7 +765,7 @@ TMRSDataTransfer SettingGravity2D(){
     sim_data.mTNumerics.m_max_iter_mixed = 3;
     sim_data.mTNumerics.m_max_iter_transport = 50;
     sim_data.mTNumerics.m_max_iter_sfi = 50;
-    sim_data.mTNumerics.m_sfi_tol = 0.001;
+    sim_data.mTNumerics.m_sfi_tol = 0.0001;
     sim_data.mTNumerics.m_res_tol_transport = 0.000001;
     sim_data.mTNumerics.m_corr_tol_transport = 0.000001;
     sim_data.mTNumerics.m_n_steps = 100;
@@ -776,7 +776,7 @@ TMRSDataTransfer SettingGravity2D(){
     std::vector<REAL> grav(3,0.0);
     grav[1] = -9.81*(1.0e-6);
     sim_data.mTNumerics.m_gravity = grav;
-    sim_data.mTNumerics.m_ISLinearKrModelQ = true;
+    sim_data.mTNumerics.m_ISLinearKrModelQ = false;
     
     
     // PostProcess controls
