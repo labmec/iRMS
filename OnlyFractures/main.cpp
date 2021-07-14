31e24074c09289f4f0d6f49ff1d0bdde5dbe0f95
Nathan Shauer <nathan.sh@gmail.com>
2021-07-14 14:44:12 -0300

    Created target to run problem with only the fractures of a dfnmesh. NOT WORKING

---
+++ OnlyFractures/main.cpp
@@ -0,0 +1,219 @@
+#ifdef HAVE_CONFIG_H
+#include <pz_config.h>
+#endif
+
+// C++ includes
+
+// PZ includes
+#include "TMRSApproxSpaceGenerator.h"
+#include "TMRSDataTransfer.h"
+#include "TMRSSFIAnalysis.h"
+#include "TMRSMixedAnalysis.h"
+#include "TMRSTransportAnalysis.h"
+#include "TPZRefPatternTools.h"
+#include "TPZReservoirTools.h"
+#include "pzlog.h"
+#ifdef USING_BOOST
+#include "boost/date_time/posix_time/posix_time.hpp"
+#endif
+
+#include "pzlog.h"
+
+// ----- Functions -----
+
+void CaseOnlyFractures();
+TMRSDataTransfer SettingFractures();
+TPZGeoMesh *ReadFractureMesh();
+
+// ----- End of Functions -----
+
+//-------------------------------------------------------------------------------------------------
+//   __  __      _      _   _   _
+//  |  \/  |    / \    | | | \ | |
+//  | |\/| |   / _ \   | | |  \| |
+//  | |  | |  / ___ \  | | | |\  |
+//  |_|  |_| /_/   \_\ |_| |_| \_|
+//-------------------------------------------------------------------------------------------------
+int main(){
+    TPZLogger::InitializePZLOG();
+    CaseOnlyFractures();
+    return 0;
+}
+// ----------------- End of Main -----------------
+
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
+
+void CaseOnlyFractures()
+{
+    // Reading ONLY de fractures of mesh from DFN
+    TPZGeoMesh *gmesh = ReadFractureMesh();
+    const bool printgmesh = true;
+    if (printgmesh) {
+        std::ofstream name("GeoMeshFractures.vtk");
+        TPZVTKGeoMesh::PrintGMeshVTK(gmesh, name);
+    }
+        
+    TMRSApproxSpaceGenerator aspace;
+    
+    // Sets the material properties and discretization parameters
+    TMRSDataTransfer sim_data  = SettingFractures();
+    
+    // Approximation space
+    sim_data.mTGeometry.mSkeletonDiv = 0;
+    sim_data.mTGeometry.m_skeletonMatId = 19;
+    sim_data.mTNumerics.m_four_approx_spaces_Q = false;
+    sim_data.mTNumerics.m_mhm_mixed_Q = false;
+    sim_data.mTNumerics.m_SpaceType = TMRSDataTransfer::TNumerics::E2Space;
+    
+    // Setting gmesh
+    aspace.SetGeometry(gmesh);
+    
+    // Setting the global data transfer
+    aspace.SetDataTransfer(sim_data);
+    
+    // Creates de multiphysics compmesh
+    int order = 1;
+    aspace.BuildMixedMultiPhysicsCompMesh(order);
+    TPZMultiphysicsCompMesh * mixed_operator = aspace.GetMixedOperator();
+    
+    // Analysis parameters
+    bool must_opt_band_width_Q = true;
+    int n_threads = 0;
+    bool UsingPzSparse = false;
+    bool UsePardiso_Q = true; // This parameter should be always "true"
+    
+    // Setting analysis
+    TMRSMixedAnalysis *mixAnalisys = new TMRSMixedAnalysis(mixed_operator, must_opt_band_width_Q);
+    mixAnalisys->SetDataTransfer(&sim_data);
+    mixAnalisys->Configure(n_threads, UsePardiso_Q, UsingPzSparse);
+    
+    // -------------- Running problem --------------
+    mixAnalisys->Assemble();
+    mixAnalisys->Solve();
+    mixed_operator->UpdatePreviousState(-1.);
+    TPZFastCondensedElement::fSkipLoadSolution = false;
+    mixed_operator->LoadSolution(mixed_operator->Solution());
+    // -------------- End of running problem --------------
+    
+    // Post processing
+    mixAnalisys->fsoltransfer.TransferFromMultiphysics();
+    mixAnalisys->PostProcessTimeStep();
+    
+    // Cleaning up
+    delete gmesh;
+}
+
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
+
+TMRSDataTransfer SettingFractures(){
+    
+    // Fracture material
+    TMRSDataTransfer sim_data;
+    sim_data.mTGeometry.mDomainDimNameAndPhysicalTag[2]["Fracture12"] = 1;
+
+    // NS: What are these?
+    sim_data.mTGeometry.mInterface_material_id = 100;
+    sim_data.mTGeometry.mInterface_material_idFracInf = 101;
+    sim_data.mTGeometry.mInterface_material_idFracSup = 102;
+    sim_data.mTGeometry.mInterface_material_idFracFrac = 103;
+    sim_data.mTGeometry.mIterface_material_idFracBound = 104;
+    
+    int D_Type = 0;
+//    int N_Type = 1;
+    REAL pressure_in = 4.0 ;
+    
+    // Boundary conditions
+    sim_data.mTBoundaryConditions.mBCMixedPhysicalTagTypeValue.Resize(1);
+    sim_data.mTBoundaryConditions.mBCMixedPhysicalTagTypeValue[0] = std::make_tuple(-1,D_Type,pressure_in);
+    
+    // Fluid Properties
+    sim_data.mTFluidProperties.mWaterViscosity = 0.1;
+    sim_data.mTFluidProperties.mOilViscosity = 0.1;
+    sim_data.mTFluidProperties.mWaterDensityRef = 1000.0;
+    sim_data.mTFluidProperties.mOilDensityRef = 1000.0;
+    
+    // Numerical controls
+    sim_data.mTNumerics.m_max_iter_mixed = 1;
+    sim_data.mTNumerics.m_max_iter_transport = 1;
+    sim_data.mTNumerics.m_max_iter_sfi = 1;
+    
+    // BorderElementOrder
+    sim_data.mTNumerics.m_BorderElementPresOrder=1;
+    sim_data.mTNumerics.m_BorderElementFluxOrder=1;
+    
+    // Other properties?
+    sim_data.mTGeometry.mSkeletonDiv = 0;
+    sim_data.mTNumerics.m_sfi_tol = 0.0001;
+    sim_data.mTNumerics.m_res_tol_transport = 0.0001;
+    sim_data.mTNumerics.m_corr_tol_transport = 0.0001;
+    sim_data.mTNumerics.m_n_steps = 1 ;
+    sim_data.mTNumerics.m_dt      = 1.0; //*day;
+    sim_data.mTNumerics.m_four_approx_spaces_Q = true;
+    sim_data.mTNumerics.m_mhm_mixed_Q          = true;
+    std::vector<REAL> grav(3,0.0);
+    grav[1] = 0.0;//-9.8*(1.0e-6); // hor
+    sim_data.mTNumerics.m_gravity = grav;
+    sim_data.mTNumerics.m_ISLinearKrModelQ = true;
+    sim_data.mTNumerics.m_nThreadsMixedProblem = 0;
+    
+    //FracAndReservoirProperties
+    sim_data.mTFracProperties.m_Permeability = 0.00001;
+    REAL kappa=1.0;
+    int  id1=1;
+    int  id2=2;
+    std::vector<std::pair<int, REAL>> idPerm(2);
+    idPerm[0]= std::make_pair(id1,kappa);
+    idPerm[1]= std::make_pair(id2,kappa);
+    sim_data.mTReservoirProperties.m_permeabilitiesbyId = idPerm;
+    
+    
+    
+    // PostProcess controls
+    sim_data.mTPostProcess.m_file_name_mixed = "mixed_operator.vtk";
+    sim_data.mTPostProcess.m_file_name_transport = "transport_operator.vtk";
+    TPZStack<std::string,10> scalnames, vecnames;
+    vecnames.Push("Flux");
+    scalnames.Push("Pressure");
+    if (sim_data.mTNumerics.m_four_approx_spaces_Q) {
+        scalnames.Push("g_average");
+        scalnames.Push("p_average");
+    }
+    sim_data.mTPostProcess.m_file_time_step = sim_data.mTNumerics.m_dt;
+    sim_data.mTPostProcess.m_vecnames = vecnames;
+    sim_data.mTPostProcess.m_scalnames = scalnames;
+    return sim_data;
+}
+
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
+
+TPZGeoMesh *ReadFractureMesh(){
+    std::string filename("../../FracMeshes/2DMeshes/2fracFromDfn.msh");
+    TPZManVector<std::map<std::string,int>,4> dim_name_and_physical_tagFine(4); // From 0D to 3D
+
+    // Fractures
+    dim_name_and_physical_tagFine[2]["Fracture12"] = 1;
+
+    // Fractures BCs
+    dim_name_and_physical_tagFine[1]["BCfrac0"] = -1;
+    dim_name_and_physical_tagFine[1]["BCfrac1"] = -1;
+    
+    // Intersections
+    dim_name_and_physical_tagFine[1]["fracIntersection_0_1"] = 13;
+        
+    // Creating gmsh reader
+    TPZGmshReader  GeometryFine;
+    TPZGeoMesh *gmeshFine;
+    REAL l = 1.0;
+    GeometryFine.SetCharacteristiclength(l);
+    GeometryFine.SetFormatVersion("4.1");
+    
+    // Reading mesh
+    GeometryFine.SetDimNamePhysical(dim_name_and_physical_tagFine);
+//    gmeshFine = GeometryFine.GeometricGmshMesh(filename,nullptr,false);
+    gmeshFine = GeometryFine.GeometricGmshMesh4(filename,nullptr,false);
+
+    return gmeshFine;
+}
