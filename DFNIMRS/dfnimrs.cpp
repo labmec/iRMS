83e9d454d85797b72ce44322315fd0fc873a6cc4
PhilippeDevloo <phil@fec.unicamp.br>
2022-05-24 09:48:23 +0200

    adjusted boundary conditions for case 8 as well

--- DFNIMRS/dfnimrs.cpp
+++ DFNIMRS/dfnimrs.cpp
@@ -77,7 +77,7 @@ int main(){
     // 6: Two parallel square fractures very close, but no overlap
     // 7: Two parallel square fractures very close, WITH overlap
     // 8: Flemisch case 3 with snapping of the bottom fracture to the middle fracture
-	int simcase = 3;
+	int simcase = 8;
     string filenameBase;
     switch (simcase) {
         case 0:
@@ -131,7 +131,7 @@ void RunProblem(string& filenameBase, const int simcase)
     // ----- Creating gmesh and data transfer -----
     TPZGeoMesh *gmeshfine = nullptr, *gmeshcoarse = nullptr;
 	ReadMeshesDFN(filenameBase, gmeshfine, gmeshcoarse, initVolForMergeMeshes);
-	if(simcase == 3) ModifyBCsForCase3(gmeshfine); // Generate the correct mesh and delete!
+	if(simcase == 3 || simcase == 8) ModifyBCsForCase3(gmeshfine); // Generate the correct mesh and delete!
     
     // ----- Printing gmesh -----
 #ifdef PZDEBUG
@@ -141,12 +141,12 @@ void RunProblem(string& filenameBase, const int simcase)
             TPZVTKGeoMesh::PrintGMeshVTK(gmeshfine, name);
             std::ofstream name2("GeoMesh_Fine_Initial.txt");
             gmeshfine->Print(name2);
-            int64_t nel = gmeshfine->NElements();
-            for (int64_t el = 0; el<nel; el++) {
-                TPZGeoEl *gel = gmeshfine->Element(el);
-                if(!gel) continue;
-                if(el != gel->Id()) std::cout << "el = " << el << " id = " << gel->Id() << std::endl;
-            }
+//            int64_t nel = gmeshfine->NElements();
+//            for (int64_t el = 0; el<nel; el++) {
+//                TPZGeoEl *gel = gmeshfine->Element(el);
+//                if(!gel) continue;
+//                if(el != gel->Id()) std::cout << "el = " << el << " id = " << gel->Id() << std::endl;
+//            }
         }
         if(gmeshcoarse){
             std::ofstream name("GeoMesh_Coarse_Initial.vtk");
@@ -889,6 +889,7 @@ void fixPossibleMissingIntersections(TMRSDataTransfer& sim_data, TPZGeoMesh* gme
                     bcEls.push_back(neigel);
                 }
             }
+            if(bcEls.size() > 1) DebugStop();
             if (interEls.size() > 1) {
                 if (interEls.size() > 2) {
                     DebugStop(); // there are 3 intersection elements in the same place! Please check why...
@@ -902,11 +903,10 @@ void fixPossibleMissingIntersections(TMRSDataTransfer& sim_data, TPZGeoMesh* gme
                 gmesh->ElementVec()[duplicateIndex] = nullptr;
                 
             }
-            if (nFracElsForSide > 1 && !interEls.size()) {
+            if (nFracElsForSide > 1 && !interEls.size() && !bcEls.size()) {
                 cout << "nfracs for this side: " << nFracElsForSide << endl;
-                cout << "Manually creating intersection geoel..." << endl;
-                TPZGeoElBC(gel, iside, matidintersect);
-                nInterCreated++;
+                cout << "This is an error" << endl;
+                DebugStop();
             }
             if ((interEls.size() || nFracElsForSide > 1) && bcEls.size()) {
                 cout << "PLEASE CHECK CAREFULLY! An element may have a boundary even if it intersects\n";
