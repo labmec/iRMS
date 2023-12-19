355e93e078ff94ee3b80728474793faeee622c3a
Nathan Shauer <nathan.sh@gmail.com>
2023-12-19 10:18:07 -0300

    feat: Added new target for filling 1m module

---
+++ Filling/filling.cpp
@@ -0,0 +1,56 @@
+// C++ includes
+// nothing
+
+// PZ includes
+#include "TPZGenGrid3D.h"
+#include "TMRSApproxSpaceGenerator.h"
+#include "imrs_config.h"
+#include "pzlog.h"
+#include "json.hpp"
+#include <filesystem>
+#include "TPZSimpleTimer.h"
+#include "pzintel.h"
+#include "pzsmanal.h"
+
+// ----- Namespaces -----
+using namespace std;
+namespace fs = std::filesystem;
+
+// ----- End of namespaces -----
+
+// ----- Global vars -----
+const int glob_n_threads = 8;
+
+// ----- Functions -----
+
+// ----- Logger -----
+#ifdef PZ_LOG
+static TPZLogger mainlogger("imrs");
+static TPZLogger fracIntersectLogger("imrs_fracIntersect");
+#endif
+
+//-------------------------------------------------------------------------------------------------
+//   __  __      _      _   _   _
+//  |  \/  |    / \    | | | \ | |
+//  | |\/| |   / _ \   | | |  \| |
+//  | |  | |  / ___ \  | | | |\  |
+//  |_|  |_| /_/   \_\ |_| |_| \_|
+//-------------------------------------------------------------------------------------------------
+int main(int argc, char* argv[]){
+    string basemeshpath(FRACMESHES);
+#ifdef PZ_LOG
+    string logpath = basemeshpath + "/../Filling/log4cxx.cfg";
+    TPZLogger::InitializePZLOG(logpath);
+    if (mainlogger.isDebugEnabled()) {
+        std::stringstream sout;
+        sout << "\nLogger for DFNIMRS problem target\n" << endl;;
+        LOGPZ_DEBUG(mainlogger, sout.str())
+    }
+#endif
+    
+    cout << "-------------------- Simulation Finished --------------------" << endl;
+    return 0;
+}
+// ----------------- End of Main -----------------
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
